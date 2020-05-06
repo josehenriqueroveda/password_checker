6b88a53a460fe31ac22cd87071bb3a85e80c7ee3
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2020-05-06 11:53:14 -0300

    Check passwords by command line

---
+++ check_password.py
@@ -0,0 +1,38 @@
+import requests
+import hashlib
+import sys
+
+def request_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error: {res.status_code} - Check the API docs and try again')
+    return res
+
+
+def get_password_leaks(hashes, hash_tail):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, leaks in hashes:
+        if h == hash_tail:
+            return leaks
+    return 0
+
+
+def check_api_passwords(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    five_chars, tail = sha1password[:5],  sha1password[5:]
+    response = request_data(five_chars)
+    return get_password_leaks(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = check_api_passwords(password)
+        if count:
+            print(f'{password} was found {count} times. You should change your password!')
+        else: 
+            print(f'{password} was NOT found. It is a secure password at the moment!')
+    return
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
